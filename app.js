6cffbcf3d1c24e92638dac0182aa130ee3f70b29
Umit Turk <uyt.turk@gmail.com>
2022-04-13 23:18:37 +0300

    express, nodemon, ejs, created templete structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+//Templete Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.status(200).render('index', {
+      page_name: "index"
+  });
+});
+app.get("/about", (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+  });
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
