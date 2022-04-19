13c2051edf8532db3a65a23785748ac6184a1721
Umit Turk <uyt.turk@gmail.com>
2022-04-19 23:50:51 +0300

    register page and hash password

---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.route('/signup').post(authController.createUser); //localhost:3000/users/signup
+
+
+
+module.exports = router;
