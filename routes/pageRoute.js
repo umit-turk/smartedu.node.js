ca89c0d0d394369116b9eb9a2842c9b7c0f2343d
Umit Turk <uyt.turk@gmail.com>
2022-04-23 14:59:51 +0300

    custom middleware

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const pageController = require('../controllers/pageController');
+const redirectMiddleware = require('../middlewares/redirectMiddleware');
 
 
 const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
-router.route('/register').get(pageController.getRegisterPage);
-router.route('/login').get(pageController.getLoginPage);
+//önce redirectmiddlewaresi ile kontrol et
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
+//önce redirectmiddlewaresi ile kontrol et
+router.route('/login').get(redirectMiddleware, pageController.getLoginPage);
 
 module.exports = router;
