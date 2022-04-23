76b61d5c34a11be857eb7316936e941c4817fe16
Umit Turk <uyt.turk@gmail.com>
2022-04-23 13:45:40 +0300

    dahsboard page and personal content

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,9 +26,9 @@ exports.loginUser = async (req, res) => {
       bcrypt.compare(password, user.password, (err, same) => {
         if (same) {
           // USER SESSION
-          //öncelikle hangi user giris yapıyor onu bul
+          //öncelikle hangi user giris yapıyor onu bul ve kullanicinin dashboard sayfasina yonlendir.
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       });
     }
@@ -44,5 +44,15 @@ exports.logoutUser = (req, res) => {
   //sessionu kapatmak istiyoruz
   req.session.destroy(() => {
     res.redirect('/');
+  })
+}
+
+exports.getDashboardPage = async (req, res) => {
+  //hangi kullanıcı giriş yapmış onu yakalayacağız.
+  //id si sessionumuzdaki user id ye eşit olan kullanıcıyı bul.
+  const user = await User.findOne({_id: req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   })
 }
