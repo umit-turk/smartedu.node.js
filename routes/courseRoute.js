a2d3577888c2d81ebfaae48c2fb48efe9b0fa8be
Umit Turk <uyt.turk@gmail.com>
2022-04-25 00:41:55 +0300

    update to course

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,9 +7,10 @@ const router = express.Router();
 router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.createCourse); //localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').put(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
-router.route('/:slug').delete(courseController.deleteCourse);
 
 
 module.exports = router;
